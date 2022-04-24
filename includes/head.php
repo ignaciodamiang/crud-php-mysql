83940e4cd8e502793f043be9a585a7380b61e14f
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-24 03:16:04 -0300

    table consulting db.

--- includes/head.php
+++ includes/head.php
@@ -2,8 +2,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- BOOTSTRAP CSS -->
+  <!-- BOOTSTRAP 5 CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- BOOTSTRAP 5 ICONS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
   <title>CRUD PHP MYSQL</title>
 </head>
--- index.php
+++ index.php
@@ -7,31 +7,64 @@
 
 <body>
   <?php include("includes/nav.php"); ?>
+
   <div class="container p-4">
     <div class="row">
       <div class="col-md-4">
         <?php if (isset($_SESSION['message'])): ?>
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
           <?= $_SESSION['message'] ?>
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         <?php 
         session_unset();
         endif; ?>
-        <div class="col md-8">
-          <div class="card card-body">
-            <form action="save.php" method="post">
-              <div class="mb-3"><input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
-              </div>
-              <div class="mb-3"><textarea name="description" rows="2" class="form-control"
-                  placeholder="Task description"></textarea>
-              </div>
-              <div class="d-grid">
-                <input type="submit" class="btn btn-success" name="save" value="Save">
-              </div>
-            </form>
-          </div>
+
+        <div class="card card-body">
+          <form action="save.php" method="post">
+            <div class="mb-3"><input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
+            </div>
+            <div class="mb-3"><textarea name="description" rows="2" class="form-control"
+                placeholder="Task description"></textarea>
+            </div>
+            <div class="d-grid">
+              <input type="submit" class="btn btn-success" name="save" value="Save">
+            </div>
+          </form>
         </div>
+      </div>
+      <div class="col md-8">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Created At</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              $query = "SELECT * FROM task";
+              $tasks = mysqli_query($conn, $query);
+            while($row = mysqli_fetch_array($tasks)): ?>
+
+            <tr>
+              <td><?php echo $row['title'] ?></td>
+              <td><?php echo $row['description'] ?></td>
+              <td><?php echo $row['created_at'] ?></td>
+              <td class="text-center">
+                <a href="edit.php?id=<?php echo $row['id'] ?>" class="btn btn-secondary">
+                  <i class="bi bi-pencil-square"></i>
+                </a>
+                <a href="delete.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">
+                  <i class="bi bi-trash"></i>
+                </a>
+              </td>
+            </tr>
+            <?php endwhile; ?>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
